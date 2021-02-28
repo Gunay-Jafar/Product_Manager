d50f6b30a4fc1e7d18a39fc51947a660ba083aaa
Gunay-Jafar <cafarligunay06@gmail.com>
2021-02-28 04:14:21 +0400

    first commit

---
+++ src/main/java/codejava/ProductService.java
@@ -0,0 +1,31 @@
+package codejava;
+
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ProductService {
+
+    private ProductRepository productRepository;
+
+    public ProductService(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    public List<Product> listAll() {
+        return productRepository.findAll();
+    }
+
+    public void save(Product product) {
+        productRepository.save(product);
+    }
+
+    public Product get(Long id) {
+        return productRepository.findById(id).get();
+    }
+
+    public void delete(Long id) {
+        productRepository.deleteById(id);
+    }
+}
